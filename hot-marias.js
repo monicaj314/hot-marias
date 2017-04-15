7b69efdf17a4fa5bcb02b097f19d0ed1e34bb125
emkamp <emily.pulskamp@gmail.com>
2017-04-15 14:13:44 -0500

    fixed html pages for tables and buttons, moved main js file into root dir, it should be names server.js and should be breaking out front end stuff into assets/javascript/script.js but that can be done later-ish

--- hot-marias.js
+++ hot-marias.js
@@ -12,36 +12,36 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(express.static(__dirname, + '/assets'));
 
 // Basic route that sends the user first to the AJAX Page
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../../index.html"));
-  
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get("/view", function(req, res) {
-  res.sendFile(path.join(__dirname, "../../view.html"));
+app.get("/view", function (req, res) {
+  res.sendFile(path.join(__dirname, "view.html"));
 });
 
-app.get("/make", function(req, res) {
-  res.sendFile(path.join(__dirname, "../../make.html"));
-});    
+app.get("/make", function (req, res) {
+  res.sendFile(path.join(__dirname, "make.html"));
+});
 
 
 // View curent reservations
-app.get("/api/reservations", function(req, res) {
+app.get("/api/reservations", function (req, res) {
   res.json(reservations);
 });
 
 // Create New Reservation - takes in JSON input
-app.post("/api/new", function(req, res) {
-  var newReservation = req.body;	
+app.post("/api/new", function (req, res) {
+  var newReservation = req.body;
   reservations.push(newReservation);
   res.json(newReservation);
-});   
-               
+});
+
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
