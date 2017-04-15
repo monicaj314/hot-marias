3ffa7b9a1e5dd3bcf54a17bebc97f71058b4c6f6
Monica Jones <monicaj314@yahoo.com>
2017-04-15 13:20:41 -0500

    backend javascript

---
+++ assets/javascript/hot-marias.js
@@ -0,0 +1,47 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+
+// Sets up the Express App
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../../index.html"));
+  
+});
+
+app.get("/view", function(req, res) {
+  res.sendFile(path.join(__dirname, "../../view.html"));
+});
+
+app.get("/make", function(req, res) {
+  res.sendFile(path.join(__dirname, "../../make.html"));
+});    
+
+
+// View curent reservations
+app.get("/api/reservations", function(req, res) {
+  res.json(reservations);
+});
+
+// Create New Reservation - takes in JSON input
+app.post("/api/new", function(req, res) {
+  var newReservation = req.body;	
+  reservations.push(newReservation);
+  res.json(newReservation);
+});   
+               
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
